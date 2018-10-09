f34ae38a41bebbf8c26b4739b32146dc5fd2ee8e
alkieu <43651654+alkieu@users.noreply.github.com>
2018-10-09 11:26:32 -0700

    Added program files for session 4

---
+++ test_module.py
@@ -0,0 +1,4 @@
+def hello_world():
+	print("Hello world, we made a module!")		#print Hello world, we made a module!
+
+	
---
+++ useful_modules.py
@@ -0,0 +1,10 @@
+import numpy as np		#the Numpy Library
+import matplotlib.pyplot as plt		#Matplotlib's pyplot
+
+import sys		#gives access to a C-like sys library
+import os		#gives access to operating system
+
+
+print(sys.argv)		#print any command line arguments, incl program
+print(os.getcwd())	#prints current working directory
+
